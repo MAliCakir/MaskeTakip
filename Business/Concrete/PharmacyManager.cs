e081ccae2a4437c9cea4673e97ca2d3b36a7dba6
Mehmet Ali ÇAKIR <mehmetali_c@aof.anadolu.edu.tr>
2024-02-06 19:14:05 +0300

    Odev

---
+++ Business/Concrete/PharmacyManager.cs
@@ -0,0 +1,33 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    internal class PharmacyManager : IApplicantService
+    {
+        public void ApplyForMask(Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CheckPerson(Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Person> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GiveMask(Person person)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WorkArround/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Business.Concrete;
+using Entities.Concrete;
+
+
+
+Person person1 = new Person();
+person1.FirstName = "Mehmet Ali";
+person1.LastName = "ÇAKIR";
+person1.DateOfBirthYear = 1982;
+person1.NationalIdendity = 21427669122;
+
+
+SelamVer(person1);
+PttManager pttManager = new PttManager(new PersonManager());
+pttManager.GiveMask(person1);
+
+static void SelamVer(Person person)
+{
+    Console.WriteLine("Merhaba " + person.FirstName + " Hoşgeldiniz.");
+}
+public class Vatandas
+{
+    public string Ad { get; set; }
+    public string Soyad { get; set; }
+
+    public int DogumYili { get; set; }
+    public long TcNo { get; set; }
+
+}
